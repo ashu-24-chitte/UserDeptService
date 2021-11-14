1e003bf404c197a4a80065b023705534aaa86bca
ashwini.chitte <ashwini.chitte@zensar.com>
2021-11-14 13:28:29 +0530

    Initial commit

---
+++ src/test/java/com/example/user/UserDeptService/UserDeptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user.UserDeptService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
